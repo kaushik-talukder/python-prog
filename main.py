3a7c5eb649fa7967bbeb9a8a94ceeb6972abe8ee
Kaushik Talukder <kaushik.talukder@gmail.com>
2022-11-20 17:46:16 -0500

    DB using python

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import sqlite3 as sl
+from tinydb import TinyDB, Query
+
+
 def main():
-    print("Hello World!")
+    con = sl.connect('/Users/kaushik/Workspace/database/relational-database.db')
+    with con:
+        data = con.execute("SELECT * FROM USERS")
+        for row in data:
+            print(row)
+    db = TinyDB('/Users/kaushik/Workspace/database/non-relational-database.json')
+    # db.insert({'name': 'Kaushik', 'age': 35})
+    user = Query()
+    print(db.search(user.age == 35))
 
 
 if __name__ == "__main__":
